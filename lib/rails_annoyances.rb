3d4d9908adc30bc53e36f6b0e6d98c4d427bdbeb
Justin Bean <justin.bean@intelivideo.com>
2014-11-17 14:28:31 -0700

    more annoyances eliminated

--- lib/rails_annoyances.rb
+++ lib/rails_annoyances.rb
@@ -7,6 +7,7 @@ module ActiveRecord
   module Core
     extend ActiveSupport::Concern
     module ClassMethods
+      # Monkeypatch - Eliminates the need to run Model.connection in Rails console.
       def inspect
         if self == Base
           super
@@ -17,7 +18,7 @@ module ActiveRecord
             super.constantize.connection
             attr_list(super)
           elsif Rails.const_defined? 'Server'
-            raise "#{super} (No DB Connection)"
+            "#{super} (call '#{super}.connection' to establish a connection)"
           end
         elsif table_exists?
           attr_list(super)
@@ -33,3 +34,12 @@ module ActiveRecord
     end
   end
 end
+
+# Auto reload codebase if an exception is found.
+begin
+rescue Exception
+  unless reload!
+    raise
+  end
+end
+
